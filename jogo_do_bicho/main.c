b76809fd3c512bf8863429cd5f37649d82c40683
Vinicius Oliveira <vinicius853o@gmail.com>
2020-06-19 18:54:31 -0300

    Update main.c

--- jogo_do_bicho/main.c
+++ jogo_do_bicho/main.c
@@ -9,27 +9,27 @@ int main(){
         M = rand() % 1000000;
 
         do{
-                printf("Informe V e N: ");
-                scanf("%f %lu ", &V, &N);
+              printf("Informe V e N: ");
+              scanf("%f %lu ", &V, &N);
 
-                if(!(V + N + M)) break;
+              if(!(V + N + M)) break;
 
-                if(N%10000 == M%1000){
-                        printf("%.2f\n", V * 3000);
-                 }else if(N%1000 == M%1000){
-                        printf("%.2f\n", V * 500);
-                 }else if(N%100 == M%100){
-                        printf("%.2f\n", V * 50);
-                 } else{
-                        unsigned long GA = ((N % 100) - 1) / 4;
-                        unsigned long  GS = ((M % 100) - 1) / 4;
+              if(N%10000 == M%1000){
+                     printf("%.2f\n", V * 3000);
+              }else if(N%1000 == M%1000){
+                     printf("%.2f\n", V * 500);
+              }else if(N%100 == M%100){
+                     printf("%.2f\n", V * 50);
+              } else{
+                 unsigned long GA = ((N % 100) - 1) / 4;
+                 unsigned long  GS = ((M % 100) - 1) / 4;
 
-                        if(GA == GS){
-                             printf("%.2f\n", V * 16);
-                        }else{
-                            printf("0.00");
-                        }
+                 if(GA == GS){
+                     printf("%.2f\n", V * 16);
+                 }else{
+                     printf("0.00");
                  }
+              }
         }while(1);
     return 0;
 }
