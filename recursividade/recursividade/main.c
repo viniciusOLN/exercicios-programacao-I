db1de4effee8795eea4ea8984382bfeed54ee18c
Vinicius Oliveira <vinicius853o@gmail.com>
2020-07-01 18:09:43 -0300

    somatório dos números até o valor de entrada

--- recursividade/recursividade/main.c
+++ recursividade/recursividade/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int sum(int a){
+
+       int soma;
+
+       if(a==0){
+            return 0;
+       }else{
+            soma = a + sum(a - 1);
+       }
+
+   return soma;
+}
+
+int main(){
+
+   int n = 0;
+
+   printf("Digite um Numero \n");
+   scanf("%i",&n);
+   printf("A soma de todos os numeros ate o que foi indicado e: %i", sum(n));
     return 0;
 }
