b537a457b0fd43f546b057c8f2c5d99134bbf258
Vinicius Oliveira <vinicius853o@gmail.com>
2020-07-01 18:57:03 -0300

    .

--- recursividade/tabuada/main.c
+++ recursividade/tabuada/main.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 
+int tabuada(int n, int end){
+    int cont;
+
+    if(n%10 ==0){
+        return 0;
+    }else{
+        cont++;
+        printf("%i \n", n);
+
+         return n * tabuada(n, (end-1));
+    }
+
+}
+
+
 int main(){
 
        int n = 0;
-
+       int limit = 10;
        printf("Digite o numero que vc deseja a tabuada\n");
        scanf("%i", &n);
 
-       tabuada(n);
+       tabuada(n,limit);
 
     return 0;
 }
