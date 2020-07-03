789a22ee18a7b56bb4f30166c5bb7192333f381e
Vinicius Oliveira <vinicius853o@gmail.com>
2020-07-03 18:43:19 -0300

    fibonacci

--- recursividade/fibonacci/main.c
+++ recursividade/fibonacci/main.c
@@ -7,8 +7,7 @@ int fibonacci(int n){
         if (n == 1 || n == 0){
             return 1;
         }else {
-             result = fibonacci(n - 1) + fibonacci(n - 2);
-            printf("%i ", n);
+             return fibonacci(n - 1) + fibonacci(n - 2);
         }
 
 }
