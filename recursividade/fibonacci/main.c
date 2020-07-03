3bc2bea8fc1d7ef716c4090e0946487ab315479b
Vinicius Oliveira <vinicius853o@gmail.com>
2020-07-03 18:29:01 -0300

    sequencia de fibonacci

--- recursividade/fibonacci/main.c
+++ recursividade/fibonacci/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 
+int fibonacci(int n){
+
+    int cont;
+    int result;
+
+    if(cont == n){
+        return n;
+    }else{
+        cont++;
+      result =   n + fibonacci(n - 1);
+      printf("%i ", result);
+    }
+
+}
+
+
 int main(){
 
+    int n = 0;
 
+    printf("Digite o digito ate que voce deseja mostrar a sequencia de fibonacci");
+    scanf("%i", &n);
+
+    fibonacci(n);
 
     return 0;
 }
