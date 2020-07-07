755d8e1a9330f92f6d391ec4a1d20c8119de4db1
Vinicius Oliveira <vinicius853o@gmail.com>
2020-07-07 17:56:15 -0300

    questão 1 do estudo de funções

--- recursividade/funcoes/main.c
+++ recursividade/funcoes/main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 
+void troca_valor(int x, int y) {
+
+    int c = x;
+    x = y;
+    y = c;
+
+    printf("x depois da troca: %i \ty depois da troca: %i",x,y);
+
+}
+
+void troca_referencia(int *x, int *y){
+
+    int c = *x;
+    *x = *y;
+    *y = c;
+
+}
+
 int main(){
 
+    int x, y = 0;
 
+    printf("Digite dois numeros ");
+    scanf("%i %i", &x, &y);
+
+    printf("\ntroca por valor \n\n");
+    printf("x antes da troca: %i \ty antes da troca: %i\t", x, y);
+
+    troca_valor(x, y);
+
+    printf("\n\n troca por referencia:\n\n");
+    printf("x antes da troca: %i \ty antes da troca: %i\t", x, y);
+    troca_referencia(&x,&y);
+    printf("x depois da troca: %i \ty depois da troca: %i",x,y);
 
     return 0;
 }
