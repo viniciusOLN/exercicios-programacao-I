13a6200fd6059297b27a696d4c60ee18ede4432d
Vinicius Oliveira <vinicius853o@gmail.com>
2020-07-13 18:56:49 -0300

    estudo de vetores em c

--- vetores/matrizes/main.c
+++ vetores/matrizes/main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 #define LIN 3
 #define COL 3
 
 int main(){
 
-    int m[LIN][COL];
+    /*int m[LIN][COL];
 
     for(int i = 0; i < LIN; i++){
         for(int j = 0; j < COL; j++){
@@ -20,6 +18,16 @@ int main(){
             printf("%3d", m[i][j]);
         }
       printf("\n");
+    }*/
+
+    int m[3][3][3];
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j <3; j++){
+            for(int k = 0; k < 3; k++){
+                scanf("%d",m[i][j][k]);
+            }
+        }
     }
 
     return 0;
