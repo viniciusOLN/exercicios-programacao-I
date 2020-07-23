c8e26b407b2dd8375c4c952c99c65199c1e164f6
Vinicius Oliveira <vinicius853o@gmail.com>
2020-07-23 18:26:20 -0300

    atividade de ponteiros

--- vetores/atividade_ponteiro/main.c
+++ vetores/atividade_ponteiro/main.c
@@ -4,27 +4,23 @@
 #define max 5
 
 int* concatenaVet (int v1[], int n1, int v2[], int n2){
-     int v3[max * 2];
+     int v3[n1 + n2];
      int *pt = &v3;
 
         for(int i = 0; i < n1; i++){
-            *pt = v1[i];
-            pt = pt + 1;
+            v3[i]= v1[i];
         }
 
         pt = 5;
         for(int i =5; i < n2; i++){
-            *pt = v2[i];
-            pt = pt + 1;
+            v3[i]= v2[i];
         }
 
-    return 0;
+    return *pt;
 }
 
 int main(){
     int v1[max], v2[max];
-    int v3[max * 2];
-
 
     srand((unsigned) time(NULL));
         for(int i = 0; i < max; i++){
@@ -35,11 +31,23 @@ int main(){
             v2[i] = rand() % 101;
         }
 
-        concatenaVet (v1,max,v2,max);
-
-        for(int i = 0; i < max*2; i++){
-            printf("%d  ", v3[i]);
+        printf("vetor 1: \n");
+        for(int i = 0; i < max; i++){
+            printf("%d ", v1[i]);
         }
+
+        printf("\nvetor 2: \n");
+        for(int i = 0; i < max; i++){
+            printf("%d ", v2[i]);
+        }
+
+        printf("\n\n");
+       int *n = concatenaVet (v1,max,v2,max);
+
+       for(int i = 0; i < max+max; i++){
+        printf("%p ", n);
+       }
+
 
     return 0;
 }
