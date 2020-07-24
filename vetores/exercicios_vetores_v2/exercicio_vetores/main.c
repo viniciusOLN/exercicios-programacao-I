167617ba559cfdb3b8f32d94ff1736ecaade37ba
Vinicius Oliveira <vinicius853o@gmail.com>
2020-07-24 19:44:01 -0300

    .

--- vetores/exercicios_vetores_v2/exercicio_vetores/main.c
+++ vetores/exercicios_vetores_v2/exercicio_vetores/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define max 5
+#define max 10
 
 void maior_valor(){
     int i, j;
@@ -83,33 +83,97 @@ void ordenar(int k[],int v[], int limit){
 void maior_jogador(){
     int jogador[max], nota[max];
 
-    carregar(nota,jogador,max);
+        carregar(nota,jogador,max);
 
     int maior = nota[0];
     int pos = 0;
 
-    for(int i = 1; i < max; i++){
-        if(nota[i] > maior){
-            maior =nota[i];
-            pos = i;
+        for(int i = 1; i < max; i++){
+            if(nota[i] > maior){
+                maior =nota[i];
+                pos = i;
+            }
         }
-    }
 
     printf("O %d jogador foi o maior com a nota de %d",jogador[pos],  maior);
 
-    ordenar(jogador,nota, max);
+        ordenar(jogador,nota, max);
 
     printf("\n");
 
-    for(int i = 0; i <3; i++){
-        printf("(%d lugar)O jogador %d teve nota de: %d\n", i+1, jogador[i], nota[i]);
+        for(int i = 0; i <3; i++){
+            printf("(%d lugar)O jogador %d teve nota de: %d\n", i+1, jogador[i], nota[i]);
+        }
+}
+
+void par(){
+    int i,j,ch = 0;
+    //printf("Digite um numero");
+    scanf("%d",&i);
+    //printf("Digite um numero");
+    //scanf("%d",&j);
+
+        if(i%2==0){
+            printf("%d ", i);
+            scanf("%d", i);
+                if(i%2==0){
+                     printf("%d ", i);
+                    scanf("%d", i);
+                }else{
+                     scanf("%d", i);
+                     if(i%2==0){
+                        printf("%d ",i);
+                     }
+                }
+        }else{
+            scanf("%d", i);
+                 if(i%2==0){
+                    printf("%d ",i);
+                 }
+        }
+
+
+    //while((ch =getchar()) != '\n'  && ch !=EOF);
+/*if((i%2==0) && (j%2==0)){
+    printf("%d ", i);
+    printf("%d ", j);
+    scanf("%d",&i);
+    scanf("%d",&j);
+}
+    if(!(i%2==0)){
+        scanf("%d", &i);
+            if(!(i%2==0)){
+                scanf("%d", &i);
+            }else{
+                printf("%d ", i);
+            }
+    }else{
+        printf("%d ", i);
     }
+
+    if(!(j%2==0)){
+        scanf("%d", &j);
+        if(!(j%2==0)){
+            scanf("%d", &j);
+        }else{
+            printf("%d ", j);
+        }
+    }else{
+        printf("%d ", j);
+    }*/
+
 }
 
 int main(){
 
+    //primeira questão
     //maior_valor();
+    //segunda questão
     //fatorial();
-    maior_jogador();
+    //terceira questão
+    //maior_jogador();
+    //quarta questão
+
+    par();
     return 0;
 }
