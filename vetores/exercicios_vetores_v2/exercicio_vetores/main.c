a886af3d9946e37d0f22e2a6fe8890ac669bbe15
Vinicius Oliveira <vinicius853o@gmail.com>
2020-07-24 17:53:57 -0300

    lista de exercicios

--- vetores/exercicios_vetores_v2/exercicio_vetores/main.c
+++ vetores/exercicios_vetores_v2/exercicio_vetores/main.c
@@ -1,9 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void maior_valor(){
+    int i, j;
+
+    printf("Digite um valor: ");
+    scanf("%d", &i);
+
+    printf("Digite um valor: ");
+    scanf("%d", &j);
+
+    if(i > j){
+        printf("Digite um valor: ");
+        scanf("%d", &j);
+    }else{
+        printf("Digite um valor: ");
+        scanf("%d", &i);
+    }
+
+    if(i > j){
+        printf("Digite um valor: ");
+        scanf("%d", &j);
+    }else{
+        printf("Digite um valor: ");
+        scanf("%d", &i);
+    }
+
+    if(i > j){
+        printf("Digite um valor: ");
+        scanf("%d", &j);
+    }else{
+        printf("Digite um valor: ");
+        scanf("%d", &i);
+    }
+
+    if(i > j){
+        printf("O maior  valor e: %d", i);
+    }else{
+        printf("O maior valor e: %d ", j);
+    }
+}
+
+void fatorial(){
+    int n = 0;
+    int a = 1;
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+  for(int i = 1; i <= n;  i++){
+     a = a * i;
+  }
+    printf("%d", a);
+}
+
+
+
 int main(){
 
-
-
+    //maior_valor();
+    //fatorial();
     return 0;
 }
