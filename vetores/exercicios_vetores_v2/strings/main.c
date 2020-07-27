2fcb3abded6975a8f82f45a3f7a62083d2dab821
Vinicius Oliveira <vinicius853o@gmail.com>
2020-07-27 17:47:29 -0300

    contando os caracteres de uma string passada por scanf

--- vetores/exercicios_vetores_v2/strings/main.c
+++ vetores/exercicios_vetores_v2/strings/main.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// %[] dentro do scanf ele lê somente os caracteres indicados dentro dos colchetes até encontrar um valor diferente do indicado
+// %[^] dentro do scanf significa negação, isto é, não lerá nada que tem dentro dos colchetes
+
+
+
 int main(){
 
     char nome[31] = "Vinicius Oliveira do Nascimento";
+    printf("%s\n", nome);
+
+    scanf("%[^\n]", nome);
     printf("%s", nome);
+
+    int i;
+
+    for(i = 0; nome[i] !='\0'; i++);
+    printf("\nQuantidade de caracteres de %s: %d\n",nome,i);
 
     return 0;
 }
