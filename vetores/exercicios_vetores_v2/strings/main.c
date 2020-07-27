9ae19784f5d180cb75af5bc1145c6927dad1fd0b
Vinicius Oliveira <vinicius853o@gmail.com>
2020-07-27 18:18:59 -0300

    strings

--- vetores/exercicios_vetores_v2/strings/main.c
+++ vetores/exercicios_vetores_v2/strings/main.c
@@ -3,13 +3,15 @@
 
 // %[] dentro do scanf ele lê somente os caracteres indicados dentro dos colchetes até encontrar um valor diferente do indicado
 // %[^] dentro do scanf significa negação, isto é, não lerá nada que tem dentro dos colchetes
+//%20[^\n] - leria somente ate 20caracteres
 //strcpy() - função de atribuição para strings. strcpy(st1, st2) - esta pegando o valor da string 2 para a string 1.
 /*strcpm() - função de comparação de strings; strcmp(st1, st2) - está comparando st1 com st2 e retorna um valor inteiro que pode ser 1, 0 ou -1.
     0 - para iguais; st1 == st2
-    1 - para uma string maior q a outra; st2 > st1
-   -1 - para uma string menor q a outra: st2< st1
+    1 - para uma string maior q a outra; st1 > st2
+   -1 - para uma string menor q a outra: st1< st2
 */
 //strlen() - le o tamanho de uma string
+// for(i = 0; nome[i] !='\0'; i++);
 
 int main(){
 
@@ -19,9 +21,9 @@ int main(){
     scanf("%[^\n]", nome);
     printf("%s", nome);
 
-    int i;
+    int i = strlen(nome);
 
-    for(i = 0; nome[i] !='\0'; i++);
+
     printf("\nQuantidade de caracteres de %s: %d\n",nome,i);
 
     return 0;
