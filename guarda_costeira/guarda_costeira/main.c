ffc64898d440d045795af088622ddf1bab03b75d
Vinicius Oliveira <vinicius853o@gmail.com>
2020-07-29 18:04:58 -0300

    atividade avaliativa

--- guarda_costeira/guarda_costeira/main.c
+++ guarda_costeira/guarda_costeira/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
 
+    int max = 0;
+    printf("quantidade de valores ");
+    scanf("%d", &max);
+    int vf[max],vg[max],d[max];
 
 
     return 0;
