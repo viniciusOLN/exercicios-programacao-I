718871c1e904039c8a740e77b9fc74f2f6b88557
Vinicius Oliveira <vinicius853o@gmail.com>
2020-07-29 22:34:04 -0300

    atividade avaliativa

--- guarda_costeira/guarda_costeira/main.c
+++ guarda_costeira/guarda_costeira/main.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void carregar(int vf[], int vg[], int d[], int max){
-
+void carregar(float vf[], float vg[], float d[], int max){
+    int a = 0;
     for(int i = 0; i < max; i++){
         printf("Digite a velocidade do fugitivo, da guarda costas e a distancia respectivamente(%d): ", i+1);
-        scanf("%d %d %d", &vf[i], &vg[i], &d[i]);
-        if(i == (max - 1)){
-            vf[max - 1] = 0;
-            vg[max - 1] = 0;
-        }
-    }
-}
-
-void mostrar(int v[], int m){
-
-    for(int i = 0; i < m; i++){
-        printf("%d ",v[i]);
+        scanf("%f %f %f", &vf[i], &vg[i], &d[i]);
     }
 
+    for(int i = 0; i < max; i++){
+        vf[i] = ((12 - d[i])/vf[i]);
+        vg[i] = 12 - vg[i];
+    }
+
+    for(int i = 0; i < max; i++){
+       if(vg[i] >= vf[i]){
+            printf("s ");
+       }else{
+            printf("n ");
+       }
+    }
 }
 
 int main(){
@@ -26,11 +27,12 @@ int main(){
     int max = 0;
     printf("quantidade de valores ");
     scanf("%d", &max);
-    int vf[max], vg[max], d[max];
+    float vf[max];
+    float vg[max];
+    float d[max];
     char result[max];
 
     carregar(vf, vg, d, max);
-    mostrar(vf, max);
 
     return 0;
 }
