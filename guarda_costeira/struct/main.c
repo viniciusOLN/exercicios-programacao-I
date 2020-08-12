36b0d8bdc9677e0ea4d8b674e0f2e5c740e95602
Vinicius Oliveira <vinicius853o@gmail.com>
2020-08-12 17:27:31 -0300

    Estudo de Structs

---
+++ guarda_costeira/struct/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Conta{
+    int numero;
+    float saldo;
+};
+
+int main(){
+
+    Conta vinicius;
+    vinicius.numero = 10;
+    vinicius.saldo = 12345;
+
+    return 0;
+}
