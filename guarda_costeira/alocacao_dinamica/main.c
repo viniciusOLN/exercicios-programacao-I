9d73678fa64388005d78fb130d9c236138a8628d
Vinicius Oliveira <vinicius853o@gmail.com>
2020-08-28 18:14:51 -0300

    Estudos - Estudando alocação dinâmica

--- guarda_costeira/alocacao_dinamica/main.c
+++ guarda_costeira/alocacao_dinamica/main.c
@@ -5,8 +5,17 @@ int main(){
 
     int *p;
 
-    p = (* int) malloc(sizeof(int));
+    p = (int*) malloc(4 * sizeof(int));
 
+    for(int i = 0; i < 4; i++){
+        p[i] = i + 11;
+    }
+
+    for(int i = 0; i < 4; i++){
+        printf("%d ", p[i]);
+    }
+
+    free(p);
 
     return 0;
 }
