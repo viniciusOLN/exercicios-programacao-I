7624761575d478dc925f2c8127e46aaa5cea2378
Vinicius Oliveira <vinicius853o@gmail.com>
2020-09-09 18:22:40 -0300

    ESTUDOS - manipulação de arquivos em C

--- manipulacao_arquivos/main.c
+++ manipulacao_arquivos/main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main(){
+    setlocale(LC_ALL, "Portuguese");
+
    FILE * fp;
+   char ch;
 
    fp = fopen("teste.txt", "r");
 
    if(fp == NULL){
         printf("deu erro");
    }else{
-        printf("deu certo");
+       do{
+            ch = fgetc(fp);
+            printf("%c", ch);
+       }while(ch !=EOF);
    }
     return 0;
 }
